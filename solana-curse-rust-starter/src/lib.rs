6e3414be9dd97d2b2dc059ce358666c4ab8c9427
jiangbo212 <peiyaoliukuan@gmail.com>
2024-01-26 17:33:33 +0800

    add movie review init

--- solana-curse-rust-starter/src/lib.rs
+++ solana-curse-rust-starter/src/lib.rs
@@ -1,4 +1,8 @@
-use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, entrypoint, msg, pubkey::Pubkey};
+pub mod instruction;
+use instruction::MoveInstruction;
+use solana_program::{account_info::AccountInfo, declare_id, entrypoint, entrypoint::ProgramResult, msg, pubkey::Pubkey};
+
+declare_id!("3qY4HbxwnR876vcUV842NY9BBgfPHHBsJ1u95QfQCf6V");
 
 entrypoint!(process_instruction);
 
@@ -7,7 +11,13 @@ pub fn process_instruction(
     accounts: &[AccountInfo],
     instruction_data: &[u8]
 ) -> ProgramResult{
-    msg!("Hello World");
+    // msg!("Hello World");
+    let instruction = MoveInstruction::unpack(instruction_data)?;
+    match instruction {
+        MoveInstruction::AddMovieReview { title, rating, description } => {
+            let _ = add_movie_review(program_id, accounts, title, rating, description);
+        }
+    }
 
     Ok(())
 }
@@ -15,6 +25,18 @@ pub fn process_instruction(
 
 pub fn add(left: usize, right: usize) -> usize {
     left + right
+}
+
+pub fn add_movie_review(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    title: String,
+    rating: u8,
+    description: String
+) -> ProgramResult {
+    msg!("Adding movie review ....");
+    msg!("Title:{}, Rating: {}, Description:{}", &title, &rating, &description);
+    Ok(())
 }
 
 #[cfg(test)]
